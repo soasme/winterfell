db67dc97733301d5922ff02786674fd8c82248ed
soasme <soasme@gmail.com>
2016-05-16 22:58:19 +0800

    import assign from utils.

--- src/reducers/player.lua
+++ src/reducers/player.lua
@@ -1,26 +1,10 @@
 local playerReducer = {}
+local utils = require('reducers/utils')
+local assign = utils.assign
 
 local initialState = {
    direction = 'UP',
 }
-
-function assign(orig, data)
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' then
-        copy = {}
-        for orig_key, orig_value in pairs(orig) do
-            copy[orig_key] = orig_value
-        end
-        for key, value in pairs(data) do
-           copy[key] = value
-        end
-    else -- number, string, boolean, etc
-        copy = orig
-    end
-    return copy
-end
-
 
 function playerReducer.reducer(state, action)
    if action.type == 'MOVE_UP' then
