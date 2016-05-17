98485f330d803b7f19e308e96fe7f6d97cd5cb71
soasme <soasme@gmail.com>
2016-05-17 21:51:47 +0800

    load fill as player.

--- src/main.lua
+++ src/main.lua
@@ -1,21 +1,27 @@
 local sti = require "sti"
+local playerReducer = require 'reducers/player'
+local worldReducer = require 'reducers/world'
+local store = {dt = 0, player = {}, world = {}}
 
 function love.load()
-    map = sti.new("assets/first-scene.lua")
-    local layer = map:addCustomLayer("Sprites", 8)
-    local player
-    for k, object in pairs(map.objects) do
-        if object.name == "Player" then
-            player = object
-            break
-        end
-    end
+   map = sti.new("assets/first-scene.lua")
 end
 
 function love.update(dt)
-    map:update(dt)
+   map:update(dt)
 end
 
 function love.draw()
-    map:draw()
+   -- draw map
+   map:draw()
+
+   -- draw text
+
+   -- draw npc
+
+   -- draw player
+   love.graphics.rectangle('fill', 0, 0, 32, 32)
 end
+
+-- call store.dispatch at event_listeners or schedule_intervals
+--
