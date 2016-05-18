01f39210d563fe8abcc81ed719d0a2b42bfe15a5
soasme <soasme@gmail.com>
2016-05-18 22:04:24 +0800

    fix up and down logic for player.

--- src/reducers/player.lua
+++ src/reducers/player.lua
@@ -12,15 +12,20 @@ function reducer(state, action)
             height = 32,
             speed = 320,
         }
+    elseif action.type == 'SET_PLAYER_POSITION' then
+        return assign(state, {
+            x = action.x or 0,
+            y = action.y or 0,
+        })
     elseif action.type == 'MOVE_UP' then
         return assign(state, {
             direction = 'UP',
-            y = state.y + state.speed * action.dt,
+            y = state.y - state.speed * action.dt,
         })
     elseif action.type == 'MOVE_DOWN' then
         return assign(state, {
             direction = 'DOWN',
-            y = state.y - state.speed * action.dt,
+            y = state.y + state.speed * action.dt,
         })
     elseif action.type == 'MOVE_LEFT' then
         return assign(state, {
