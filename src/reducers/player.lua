88c9d1e036da497677255b52afa7235719e3b78e
soasme <soasme@gmail.com>
2016-05-18 21:23:13 +0800

    test player x and y.

--- src/reducers/player.lua
+++ src/reducers/player.lua
@@ -1,32 +1,40 @@
-local player = {}
 local utils = require('reducers/utils')
 local assign = utils.assign
 
- player.initialState = {
-    direction = 'UP',
-    isWalking = false,
-}
-
-function player.reducer(state, action)
-   if action.type == 'MOVE_UP' then
-      return assign(state, {
-         direction = 'UP'
-      })
-   elseif action.type == 'MOVE_DOWN' then
-      return assign(state, {
-         direction = 'DOWN'
-      })
-   elseif action.type == 'MOVE_LEFT' then
-      return assign(state, {
-         direction = 'LEFT'
-      })
-   elseif action.type == 'MOVE_RIGHT' then
-      return assign(state, {
-         direction = 'RIGHT'
-      })
-   else
-      return state
-   end
+function reducer(state, action)
+    if action.type == '__INIT__' then
+        return {
+            direction = 'UP',
+            isWalking = false,
+            x = 0,
+            y = 0,
+            width = 32,
+            height = 32,
+            speed = 320,
+        }
+    elseif action.type == 'MOVE_UP' then
+        return assign(state, {
+            direction = 'UP',
+            y = state.y + state.speed * action.dt,
+        })
+    elseif action.type == 'MOVE_DOWN' then
+        return assign(state, {
+            direction = 'DOWN',
+            y = state.y - state.speed * action.dt,
+        })
+    elseif action.type == 'MOVE_LEFT' then
+        return assign(state, {
+            direction = 'LEFT',
+            x = state.x - state.speed * action.dt,
+        })
+    elseif action.type == 'MOVE_RIGHT' then
+        return assign(state, {
+            direction = 'RIGHT',
+            x = state.x + state.speed * action.dt,
+        })
+    else
+        return state
+    end
 end
 
-return player
+return reducer
--- src/reducers/player_spec.lua
+++ src/reducers/player_spec.lua
@@ -2,28 +2,47 @@ local playerReducer = require('reducers/player')
 
 describe("player reducer", function()
 
-            it("should do nothing responding unknown action", function()
-                  local state = playerReducer.reducer({direction = 'UP'}, {})
-                  assert.are.equal(state.direction, 'UP')
-            end)
+    function initState()
+        return playerReducer({}, {type = "__INIT__"})
+    end
 
-            it("should change direction to UP", function()
-                  local state = playerReducer.reducer({}, {type = 'MOVE_UP'})
-                  assert.are.equal(state.direction, 'UP')
-            end)
+    it("should change direction to UP", function()
+        local state = playerReducer(initState(), {
+            type = 'MOVE_UP',
+            dt = 0.1,
+        })
+        assert.are.equal(state.direction, 'UP')
+        assert.are.equal(state.x, 0)
+        assert.are.equal(state.y, 32)
+    end)
 
-            it("should change direction to DOWN", function()
-                  local state = playerReducer.reducer({}, {type = 'MOVE_DOWN'})
-                  assert.are.equal(state.direction, 'DOWN')
-            end)
+    it("should change direction to DOWN", function()
+        local state = playerReducer(initState(), {
+            type = 'MOVE_DOWN',
+            dt = 0.1,
+        })
+        assert.are.equal(state.direction, 'DOWN')
+        assert.are.equal(state.x, 0)
+        assert.are.equal(state.y, -32)
+    end)
 
-            it("should change direction to LEFT", function()
-                  local state = playerReducer.reducer({}, {type = 'MOVE_LEFT'})
-                  assert.are.equal(state.direction, 'LEFT')
-            end)
+    it("should change direction to LEFT", function()
+        local state = playerReducer(initState(), {
+            type = 'MOVE_LEFT',
+            dt = 0.1,
+        })
+        assert.are.equal(state.direction, 'LEFT')
+        assert.are.equal(state.x, -32)
+        assert.are.equal(state.y, 0)
+    end)
 
-            it("should change direction to RIGHT", function()
-                  local state = playerReducer.reducer({}, {type = 'MOVE_RIGHT'})
-                  assert.are.equal(state.direction, 'RIGHT')
-            end)
+    it("should change direction to RIGHT", function()
+        local state = playerReducer(initState(), {
+            type = 'MOVE_RIGHT',
+            dt = 0.1,
+        })
+        assert.are.equal(state.direction, 'RIGHT')
+        assert.are.equal(state.x, 32)
+        assert.are.equal(state.y, 0)
+    end)
 end)
