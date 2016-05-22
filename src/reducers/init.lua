cd2b78ed821db129afcc7c46198d852a0b58baeb
soasme <soasme@gmail.com>
2016-05-22 12:58:24 +0800

    load map reducer.

--- src/reducers/init.lua
+++ src/reducers/init.lua
@@ -1,11 +1,13 @@
 local playerReducer = require('reducers/player')
 local cameraReducer = require('reducers/camera')
+local mapReducer = require('reducers/map')
 
 function combine(state, action)
     state = state or {}
     return {
         player = playerReducer(state.player, action),
         camera = cameraReducer(state.camera, action),
+        map = mapReducer(state.map, action),
     }
 end
 
