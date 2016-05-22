de9c21972bcee6bcd28b87192d38884f3d30ebc6
soasme <soasme@gmail.com>
2016-05-22 10:59:28 +0800

    load message.

--- src/main.lua
+++ src/main.lua
@@ -1,24 +1,29 @@
+
 require('core')
 
 local cameraComponent = require('components/camera')
 local playerComponent = require('components/player')
 local mapComponent = require('components/map')
+local messageComponent = require('components/message')
 
 function love.load()
   mapComponent.load()
   cameraComponent.load()
   playerComponent.load()
+  messageComponent.load()
 end
 
 function love.update(dt)
     mapComponent.update(dt)
     playerComponent.update(dt)
     cameraComponent.update(dt)
+    messageComponent.update(dt)
 end
 
 function love.draw()
     cameraComponent.draw({
         mapComponent.draw,
         playerComponent.draw,
+        messageComponent.draw,
     })
 end
